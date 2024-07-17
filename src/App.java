7d1700e2d49711c38dc6d7b1cc2a993f1975afc4
Janaina <yanainavieira@gmail.com>
2024-07-17 16:18:54 -0300

    code final

--- src/App.java
+++ src/App.java
@@ -2,23 +2,25 @@ import javax.swing.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // variaveis da janela
-        int quadradoTamanho = 32;
-        int linhas = 16;
-        int colunas = 16;
-        int boardLargura= quadradoTamanho * colunas; 
-        int boardAltura =  quadradoTamanho * linhas;
+        //window variables
+        int tileSize = 32;
+        int rows = 16;
+        int columns = 16;
+        int boardWidth = tileSize * columns; // 32 * 16 = 512px
+        int boardHeight = tileSize * rows; // 32 * 16 = 512px
 
         JFrame frame = new JFrame("Space Invaders");
-        frame.setVisible(true);
-        frame.setSize(boardAltura, boardLargura);
+        // frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
-        frame.setResizable(false); //o usuario nao consegue mudar o tamanho da janela
+        frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        SpaceInvaders spaceInvanders = new SpaceInvaders();
-        frame.add(spaceInvanders);
+        SpaceInvaders spaceInvaders = new SpaceInvaders();
+        frame.add(spaceInvaders);
         frame.pack();
+        spaceInvaders.requestFocus();
         frame.setVisible(true);
+
     }
 }
